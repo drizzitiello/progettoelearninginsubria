d22b07ce0ebe75ea0f449d8d2b11016c63fa10d1
macri <marco97.orange@hotmail.it>
2018-11-25 21:35:21 +0100

    a

--- sa/src/gestioneContenutiCorso/GestioneContenutoCorso.java
+++ sa/src/gestioneContenutiCorso/GestioneContenutoCorso.java
@@ -6,13 +6,13 @@ import socketDb.SocketDb;
 
 public class GestioneContenutoCorso {
 	SocketDb socket;
-	public void uploadMatCorso(Corso cor, Contenuto ... con) throws ClassNotFoundException, SQLException {
+	public void uploadMatCorso(Corso cor, Contenuto ... con) throws Exception {
 		socket=SocketDb.getInstanceDb();
 		for(Contenuto cont : con) {
 			cor.setContenuti(cont,cor);
 		}
 	}
-	public void createFolder(String name, String path, int codSezione, String pubblica, String codRisorsa, String descr, String tipo) throws ClassNotFoundException, SQLException {
+	public void createFolder(String name, String path, int codSezione, String pubblica, String codRisorsa, String descr, String tipo) throws Exception {
 		socket=SocketDb.getInstanceDb();
 		String sql = "INSERT INTO Risorsa(codiceRisorsa, nome, descrizione, percorso, tipo, "
 				+ "codiceSezione, isPubblica) "
@@ -23,18 +23,18 @@ public class GestioneContenutoCorso {
 	public void cancelFolder(int codRisorsa) throws ClassNotFoundException, SQLException {
 		socket=SocketDb.getInstanceDb();
 		String sql = "DELETE FROM Risorsa"
-				+ "WHERE codRisorsa="+codRisorsa;
+				+ " WHERE codRisorsa = "+codRisorsa;
 		socket.query(sql);
 	}
-	public void modificaTitolo(int codSezione, String titolo) throws ClassNotFoundException, SQLException {
+	public void modificaTitolo(int codSezione, String titolo) throws Exception {
 		socket=SocketDb.getInstanceDb();
 		String sql = "UPDATE Sezione"+ " SET titolo="+titolo+
-				" WHERE codSezione="+codSezione;
+				" WHERE codSezione = "+codSezione;
 		socket.query(sql);
 	}
 	public void modificaVisibilita(int codSezione) throws ClassNotFoundException, SQLException {
 		socket=SocketDb.getInstanceDb();
-		String sql = "UPDATE Sezione"+ "SET isPubblica=true"+ "WHERE codSezione="+codSezione;
+		String sql = "UPDATE Sezione"+ " SET isPubblica=true"+ " WHERE codSezione = "+codSezione;
 		socket.query(sql);
 	}
 	public void visualizaAsStudent() {
